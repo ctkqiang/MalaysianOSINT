ec1cd65fc1fef69f609b242078bdbcc512aa03f8
🇨🇳钟智强 『江西青垣科技』 <johnmelodymel@qq.com>
2025-09-06 19:06:53 +0800

    feat: 添加社交平台验证功能的基础头文件 [Feature] 添加社交媒体扫描功能 #21

---
+++ include/social.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#ifndef SOCIAL_H
+#define SOCIAL_H
+
+#include <stddef.h>
+
+typedef struct {
+    const char *name;
+    const char *url_template; 
+} SocialTarget;
+
+
+void init_curl();
+void cleanup_curl();
+int check_email(const SocialTarget *target, const char *email);
+
+#endif
